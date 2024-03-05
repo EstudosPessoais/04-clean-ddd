5c624aa12bad6d774fecca2ab5f5bd5c72da6675
bielb1b2 <gabrielvt.souza@hotmail.com>
2024-03-04 22:59:48 -0300

    feat: adding getters and setters

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -2,6 +2,7 @@ import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
 import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 import { Optional } from "../../core/types/optional"
+import dayjs from 'dayjs';
 
 interface QuestionProps {
     title: string, 
@@ -13,9 +14,69 @@ interface QuestionProps {
     updatedAt?: Date 
 }
 export class Question extends Entity<QuestionProps> {
-  static create(props: Optional<QuestionProps, 'createdAt'>, id?: UniqueEntityID) {
+
+    get title(){
+        return this.props.title
+    }
+
+    get bestAnswerId(){
+        return this.props.bestAnswerId
+    }
+
+    get content(){
+        return this.props.content
+    }
+
+    get slug(){
+        return this.props.slug
+    }
+
+
+    get authorId(){
+        return this.props.authorId
+    }
+
+    get createdAt(){
+        return this.props.createdAt
+    }
+
+    get updatedAt(){
+        return this.props.updatedAt
+    }
+
+    get isNew(): Boolean {
+        return dayjs().diff(this.createdAt, 'days') <= 3
+    }
+
+    private touch() {
+        this.props.updatedAt = new Date()
+    }
+
+    get except() {
+        return this.content.substring(0, 120).trimEnd().concat('...')
+    }
+
+    set content(content: string) {
+        this.props.content = content
+        this.touch()
+    }
+
+    set title(title: string) {
+        this.props.title = title
+        this.props.slug = Slug.createFromText(title)
+        this.touch()
+    }
+
+    set bestAnswerId(bestAnswerId: UniqueEntityID) {
+        this.props.bestAnswerId = bestAnswerId
+        this.touch()
+    }
+
+
+  static create(props: Optional<QuestionProps, 'createdAt' | 'slug'>, id?: UniqueEntityID) {
     const question = new Question({
         ...props,
+        slug: props.slug ?? Slug.createFromText(props.title),
         createdAt: new Date()
     }, id)
 
