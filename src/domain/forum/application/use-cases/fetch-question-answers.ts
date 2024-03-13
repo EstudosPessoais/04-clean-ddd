fd322ab9e4c8dfa2736e9f61387bce4136943b76
bielb1b2 <gabrielvt.souza@hotmail.com>
2024-03-13 00:14:43 -0300

    feat: fetch question answers

---
+++ src/domain/forum/application/use-cases/fetch-question-answers.ts
@@ -0,0 +1,31 @@
+import { Answer } from '../../enterprise/entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
+
+interface FetchQuestionAnswersCaseRequest {
+  questionId: string
+  page: number
+}
+
+interface FetchQuestionAnswersCaseResponse {
+  answers: Answer[]
+}
+
+export class FetchQuestionAnswersCase {
+  constructor(private answersRepository: AnswersRepository) {}
+
+  async execute({
+    page,
+    questionId,
+  }: FetchQuestionAnswersCaseRequest): Promise<FetchQuestionAnswersCaseResponse> {
+    const answers = await this.answersRepository.findManyByQuestionId(
+      questionId,
+      {
+        page,
+      },
+    )
+
+    return {
+      answers,
+    }
+  }
+}
