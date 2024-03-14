2460ba97efc2de611b0825fc2d05627af6d5a83a
bielb1b2 <gabrielvt.souza@hotmail.com>
2024-03-14 09:56:55 -0300

    chore: add types to either

--- src/core/either.ts
+++ src/core/either.ts
@@ -1,22 +1,38 @@
 // Error
-export class Left<L> {
+export class Left<L, R> {
   readonly value: L
 
   constructor(value: L) {
     this.value = value
   }
+
+  isRight(): this is Rigth<L, R> {
+    return false
+  }
+
+  isLeft(): this is Left<L, R> {
+    return true
+  }
 }
 
 // Success
-export class Rigth<R> {
+export class Rigth<L, R> {
   readonly value: R
 
   constructor(value: R) {
     this.value = value
   }
+
+  isRight(): this is Rigth<L, R> {
+    return true
+  }
+
+  isLeft(): this is Rigth<L, R> {
+    return false
+  }
 }
 
-export type Either<L, R> = Left<L> | Rigth<R>
+export type Either<L, R> = Left<L, R> | Rigth<L, R>
 
 export const left = <L, R>(value: L): Either<L, R> => {
   return new Left(value)
