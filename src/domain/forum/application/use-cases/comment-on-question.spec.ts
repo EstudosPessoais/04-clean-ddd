0e952e88b0cdf31532107dab1163fe5aeb0e1ecc
bielb1b2 <gabrielvt.souza@hotmail.com>
2024-03-13 23:59:04 -0300

    feat: add question on comment use case

---
+++ src/domain/forum/application/use-cases/comment-on-question.spec.ts
@@ -0,0 +1,38 @@
+import { expect } from 'vitest'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { makeQuestion } from 'test/factories/make-question'
+import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository'
+import { CommentOnQuestionUseCase } from './comment-on-question'
+
+// SUT: System Unit Test
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
+let sut: CommentOnQuestionUseCase
+
+describe('Comment on Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new CommentOnQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionCommentsRepository,
+    )
+  })
+
+  it('should be able to comment on question', async () => {
+    const question = makeQuestion()
+
+    await inMemoryQuestionsRepository.create(question)
+
+    await sut.execute({
+      questionId: question.id.toString(),
+      authorId: question.authorId.toString(),
+      content: 'Comentario teste',
+    })
+
+    expect(inMemoryQuestionCommentsRepository.items[0].content).toEqual(
+      'Comentario teste',
+    )
+  })
+})
