3ba17b1718f385a2f9f32e9c167d85520c84f8bc
bielb1b2 <gabrielvt.souza@hotmail.com>
2024-03-14 00:34:17 -0300

    chore: fix the name of the file question repository

--- src/domain/forum/application/use-cases/choose-question-best-answer.ts
+++ src/domain/forum/application/use-cases/choose-question-best-answer.ts
@@ -1,6 +1,6 @@
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 import { Question } from '../../enterprise/entities/question'
-import { QuestionsRepository } from '../repositories/question-repository'
+import { QuestionsRepository } from '../repositories/questions-repository'
 
 interface ChooseQuestionBestAnswerRequest {
   authorId: string
