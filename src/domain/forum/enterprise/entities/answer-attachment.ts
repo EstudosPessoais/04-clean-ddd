85699af2dea1279f1601fd66d199734e7397c559
bielb1b2 <gabrielvt.souza@hotmail.com>
2024-03-28 09:14:25 -0300

    chore: to integrate with new attachments answer repository

--- src/domain/forum/enterprise/entities/answer-attachment.ts
+++ src/domain/forum/enterprise/entities/answer-attachment.ts
@@ -1,18 +1,18 @@
 import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
-interface AnswerAttachmentProps {
+export interface AnswerAttachmentProps {
   answerId: UniqueEntityID
   attachmentId: UniqueEntityID
 }
 
 export class AnswerAttachment extends Entity<AnswerAttachmentProps> {
-  get topicId() {
+  get answerId() {
     return this.props.answerId
   }
 
   get attachmentId() {
-    return this.props.answerId
+    return this.props.attachmentId
   }
 
   static create(props: AnswerAttachmentProps, id?: UniqueEntityID) {
